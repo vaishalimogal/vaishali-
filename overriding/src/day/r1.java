46407a076de13456cace48782e5a1ad96dd4aebf
vaish <vaish@DESKTOP-42DRH0O>
2023-02-06 15:07:57 +0530

    commit

---
+++ overriding/src/day/r1.java
@@ -0,0 +1,20 @@
+package day;
+
+public class r1 {
+	
+		public void j1(int q, String r) {
+			
+			System.out.println("Can you");
+		}
+		public static void main(String[] args) 
+		{
+			// TODO Auto-generated method stub
+			
+			r1 g = new r1();
+			g.j1(76,"blue");
+
+		}
+
+	}
+
+	
