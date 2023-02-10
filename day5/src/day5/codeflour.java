d05b6473f515726efdda7d45887b28539f621466
vaish <vaish@DESKTOP-42DRH0O>
2023-02-10 14:21:03 +0530

    commit

---
+++ day5/src/day5/codeflour.java
@@ -0,0 +1,21 @@
+package day5;
+
+public class codeflour {
+	
+	int a=45;
+	String r="narika";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		codeflour b=new codeflour();
+		
+		System.out.println(b.a);
+		System.out.println(b.r);
+	   }
+}
+		
+		
+
+	
+
+
